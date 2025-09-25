780e1e7a7dc62610e20ff97387a2a6fd59e1ebba
salahudeen abdulkareem <salayo4real@gmail.com>
2025-09-25 07:24:31 +0100

    userpage updated

--- alx-project-0x02/pages/users.tsx
+++ alx-project-0x02/pages/users.tsx
@@ -1,34 +1,39 @@
-import React, {useState, useEffect} from "react";
-import  { UserProps } from "@/interfaces";
+import React from "react";
+import { GetStaticProps } from "next";
+import { UserProps } from "@/interfaces";
 import UserCard from "../components/common/UserCard";
 
+interface UsersPageProps {
+  users: UserProps[];
+}
 
-const Users: React.FC = () => {
-    const [users, setUsers] = useState<UserProps[]>([]);
+const Users: React.FC<UsersPageProps> = ({ users }) => {
+  return (
+    <div className="max-w-2xl mx-auto p-6">
+      <h1 className="text-2xl font-bold mb-4">Users</h1>
+      {users.map((user) => (
+        <UserCard
+          key={user.id}
+          id={user.id}
+          email={user.email}
+          name={user.name}
+          address={user.address}
+        />
+      ))}
+    </div>
+  );
+};
 
-    useEffect(() => {
-        const fetchUsers = async () => {
-            const res = await fetch("https://jsonplaceholder.typicode.com/users");
-            const data: UserProps[] = await res.json();
-            setUsers(data);
-        };
+// Next.js will call this **at build time**
+export const getStaticProps: GetStaticProps = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const users: UserProps[] = await res.json();
 
-        fetchUsers();
-    }, []);
-
-    return (
-        <div className="max-w-2xl mx-auto p-6">
-            <h1 className="text-2xl font-bold mb-4">Users</h1>
-            {users.map((user) => (
-                <UserCard key={user.id} 
-                id= {user.id}
-                 email= {user.email}
-                 name= {user.name}
-                 address= {user.address}
-                />
-            ))}
-        </div>
-    );
+  return {
+    props: {
+      users, // this gets passed as props to the page component
+    },
+  };
 };
 
 export default Users;
